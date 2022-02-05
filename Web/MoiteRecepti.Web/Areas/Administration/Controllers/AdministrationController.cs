0c31d8517b96aebc5cdaf39917d134c8d5b9dcef
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-02-05 16:34:27 +0200

    Initial commit

---
+++ Web/MoiteRecepti.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace MoiteRecepti.Web.Areas.Administration.Controllers
+{
+    using MoiteRecepti.Common;
+    using MoiteRecepti.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
