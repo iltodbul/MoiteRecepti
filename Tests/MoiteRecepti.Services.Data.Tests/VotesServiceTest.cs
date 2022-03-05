ad23faa2a2efca05f1ed91edf206c27057867e39
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-03-05 14:00:57 +0200

    Add test with Mocking

--- Tests/MoiteRecepti.Services.Data.Tests/VotesServiceTest.cs
+++ Tests/MoiteRecepti.Services.Data.Tests/VotesServiceTest.cs
@@ -33,5 +33,26 @@ namespace MoiteRecepti.Services.Data.Tests
             Assert.Single(list);
             Assert.Equal(5, list.First().Value);
         }
+
+        [Fact]
+        public async Task When2UsersVoteForTheSameRecipeTheAverageVoteShouldBeCorrect()
+        {
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+
+            var service = new VotesService(mockRepo.Object);
+
+            await service.SetVoteAsync(1, "1", 1);
+            await service.SetVoteAsync(1, "2", 5);
+            await service.SetVoteAsync(1, "1", 2);
+
+            mockRepo.Verify(x => x.All(), Times.Exactly(3));
+            mockRepo.Verify(x => x.AddAsync(It.IsAny<Vote>()), Times.Exactly(2));
+
+            var average = service.GetAverageVote(1);
+            Assert.Equal(3.5, average);
+        }
     }
 }
